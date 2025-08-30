982bb7063276725fe04f9eb6fc037e4135f22c5f
Aniket <144130684+Aniketsharma09@users.noreply.github.com>
2025-08-30 18:44:14 +0530

    Completed

--- src/app.js
+++ src/app.js
@@ -2,21 +2,29 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
+const path = require("path");
 
 /* Require route*/
-const authRoutes = require('./routes/auth.routes');
-const chatRoutes = require('./routes/chat.routes');
+const authRoutes = require("./routes/auth.routes");
+const chatRoutes = require("./routes/chat.routes");
 // cours policy
-const cors = require('cors');
-app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true, 
-}));
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
 // using the routes
-app.use('/api/auth',authRoutes);
-app.use('/api/chat', chatRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = app;
