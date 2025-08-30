b0606ee856a70163a76ef22d9a756659bdab7861
Aniket <144130684+Aniketsharma09@users.noreply.github.com>
2025-08-30 18:22:19 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,11 @@
 const app = require("./src/app");
 const connectToDB = require("./src/db/db");
-const initSocketServer = require('./src/sockets/socket.server');
-const httpServer = require('http').createServer(app);
+const initSocketServer = require("./src/sockets/socket.server");
+const httpServer = require("http").createServer(app);
 
 initSocketServer(httpServer);
-
-httpServer.listen(3000, (req, res) => {
-    console.log("server is runing on the port 3000");
-})
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, (req, res) => {
+  console.log("server is runing on the port 3000");
+});
 connectToDB();
